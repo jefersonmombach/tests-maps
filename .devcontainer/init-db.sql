5a8679f0a3d2c2d16e0729cb09a4cef1ff8770ef
jefersonmombach <jeferson@msousa.net>
2026-02-23 16:05:44 -0300

    .

--- .devcontainer/init-db.sql
+++ .devcontainer/init-db.sql
@@ -1,7 +1,7 @@
 -- Criar extensões necessárias
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS postgis_topology;
-CREATE EXTENSION IF NOT EXISTS uuid-ossp;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Criar schema público se não existir
 CREATE SCHEMA IF NOT EXISTS public;
